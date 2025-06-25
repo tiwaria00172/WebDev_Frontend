4254bbed5261ce3374e6f7092a5c9a2bdaf60fab
tiwaria00172 <tiwariabhishek00172@gmail.com>
2025-06-25 22:00:00 +0530

    Add files via upload

---
+++ Start.js
@@ -0,0 +1,17 @@
+/*functions */
+function greet(names){
+    return "hello "+ names;
+
+}
+console.log(greet("alisha"));
+
+/*return*/
+function add(x,y){
+    return x + y;
+
+}
+console.log(add(5,6));
+/*Loops*/
+for(let i =0; i<5; i++){
+    console.log("Abhishek is great");
+}
